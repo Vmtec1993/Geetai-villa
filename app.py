3646c1cd239caa2556a8ce799ab3d8289ac21a1d
Vmtec1993 <vchamp67@gmail.com>
2026-02-18 16:51:43 +0530

    app.py

--- app.py
+++ app.py
@@ -80,6 +80,8 @@ def submit_inquiry():
         return f"Form Error: {str(e)}"
 
 if __name__ == "__main__":
+    # रेंडर के लिए पोर्ट सेट करना
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
     
